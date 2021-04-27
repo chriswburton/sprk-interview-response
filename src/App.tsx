993a487166b12f2df60da0e7a17ab5fdbfa2c86e
Chris Burton <chrisburton@chriss-mbp.home>
2021-04-27 10:20:37 +0100

    restrict dashboard route + update disabled button style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React, { FC } from 'react'
+import {
+    BrowserRouter,
+    Switch,
+    Route,
+    Redirect,
+    RouteProps,
+} from 'react-router-dom'
 import { Login } from './pages/Login/Login'
 import { Dashboard } from './pages/Dashboard/Dashboard'
+import { ACCESS_TOKEN } from './interfaces/local-storage.constants'
 
+type Props = RouteProps & { component: FC<any> }
+
+const PrivateRoute = ({ component: Component, ...rest }: Props) => {
+    return (
+        <Route
+            {...rest}
+            render={(props: any) =>
+                localStorage.getItem(ACCESS_TOKEN) ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect to={'/login'} />
+                )
+            }
+        />
+    )
+}
 function App() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path={'/login'} component={Login} />
-                <Route path={'/'} component={Dashboard} />
+                <PrivateRoute path={'/'} component={Dashboard} />
             </Switch>
         </BrowserRouter>
     )
