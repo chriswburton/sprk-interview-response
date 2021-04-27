b5709b6fb8f0863b14f01e74b8684cacc45cb0d2
Chris Burton <chrisburton@chriss-mbp.home>
2021-04-27 10:15:19 +0100

    add dashboard page

--- src/pages/Dashboard/Dashboard.test.tsx
+++ src/pages/Dashboard/Dashboard.test.tsx
@@ -1,9 +1,41 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent, waitFor } from '@testing-library/react'
 import { Dashboard } from './Dashboard'
+import { USER_NAME } from '../../interfaces/local-storage.constants'
+import { Router } from 'react-router-dom'
+
+const name = 'John'
 
 describe('Dashboard', () => {
-    it('should render successfully', () => {
-        render(<Dashboard />)
+    beforeEach(() => {
+        localStorage.clear()
+        localStorage.setItem(USER_NAME, name)
+    })
+
+    it('should display a personalised greeting', () => {
+        const { getByTestId } = render(<Dashboard />)
+        const greeting = getByTestId('greeting')
+
+        expect(greeting.textContent).toBe(`Hi ${name}! You are now logged in.`)
+    })
+
+    it('should log the user out when the button is pressed', async () => {
+        const historyMock = {
+            push: jest.fn(),
+            location: {},
+            listen: jest.fn(),
+        } as any
+        const { getByTestId } = render(
+            <Router history={historyMock}>
+                <Dashboard />
+            </Router>
+        )
+
+        const logoutButton = getByTestId('logout')
+        fireEvent.click(logoutButton)
+
+        await waitFor(() =>
+            expect(historyMock.push).toHaveBeenCalledWith('/login')
+        )
     })
 })
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,39 @@
 import { FC } from 'react'
+import { ButtonPrimary } from '../../components/ButtonPrimary'
+import { useHistory } from 'react-router'
+import { api } from '../../api'
+import { USER_NAME } from '../../interfaces/local-storage.constants'
 
 export const Dashboard: FC = () => {
-    return <>Dashboard</>
+    const history = useHistory()
+    const name = localStorage.getItem(USER_NAME)
+
+    const handleSignOut = () => {
+        api.clearAuthToken()
+        localStorage.clear()
+        history.push('/login')
+    }
+
+    return (
+        <>
+            <div className={'w-screen h-screen bg-blue-400 text-center p-8'}>
+                <div className={'animate__animated animate__fadeIn'}>
+                    <div
+                        className={
+                            'p-3 mb-4 bg-2 bg-green-500 text-white rounded-lg'
+                        }
+                        data-testid={'greeting'}
+                    >
+                        Hi {name}! You are now logged in.
+                    </div>
+                    <ButtonPrimary
+                        onClick={handleSignOut}
+                        data-testid={'logout'}
+                    >
+                        Logout
+                    </ButtonPrimary>
+                </div>
+            </div>
+        </>
+    )
 }
